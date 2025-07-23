85c93df5176aa09bbc792c16d17a6306dc9e160b
SharwanKunwar <sharwankunwar07@gmail.com>
2025-07-24 01:14:18 +0545

    updated loader 01 animation using motion

--- app/LoadersComponents/loader01/page.jsx
+++ app/LoadersComponents/loader01/page.jsx
@@ -1,8 +1,28 @@
+// components/Loader01.jsx
+'use client';
+
+import { motion } from 'framer-motion';
 
 export default function Loader01() {
   return (
     <div className="text-black text-3xl w-full h-full flex justify-center items-center">
-      aasdfasdf
+      <div className="bg-white/30 backdrop-blur-md border border-white/30 max-w-5xl min-w-5xl py-20 flex justify-center items-center rounded gap-1">
+        <motion.div 
+          animate={{ y: [0, -20, 0] }}
+          transition={{ repeat: Infinity, duration: 1, delay: 0 }}
+          className="bg-white border border-black/30 w-[40px] h-[40px] rounded-full"
+        />
+        <motion.div 
+          animate={{ y: [0, -20, 0] }}
+          transition={{ repeat: Infinity, duration: 1, delay: 0.2 }}
+          className="bg-white border border-black/30 w-[40px] h-[40px] rounded-full"
+        />
+        <motion.div 
+          animate={{ y: [0, -20, 0] }}
+          transition={{ repeat: Infinity, duration: 1, delay: 0.4 }}
+          className="bg-white border border-black/30 w-[40px] h-[40px] rounded-full"
+        />
+      </div>
     </div>
   );
 }
