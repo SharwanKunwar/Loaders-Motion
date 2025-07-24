918200a907cfef3384aaa41808abd652318299aa
SharwanKunwar <sharwankunwar07@gmail.com>
2025-07-24 17:15:10 +0545

    updated

---
+++ app/dashboard/Loading.jsx
@@ -0,0 +1,45 @@
+'use client';
+
+import React from 'react'
+import {motion} from 'motion/react';
+
+function Loading() {
+
+    return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+
+    <div className='bg-black w-[90%] h-[90%] rounded-md flex justify-center items-center relative'>
+        <motion.div 
+        animate={{rotate:0}}
+        transition={{repeat:Infinity,type:'spring', ease:'easeInOut'}}
+        className='border border-white/30 w-[200px] h-[200px] rounded-full z-20 flex justify-center items-center'>
+
+        <motion.div 
+        animate={{rotate:[360,45]}}
+        transition={{repeat:Infinity,duration:1,type:'spring', ease:'easeInOut'}}
+        className='border border-white/30 w-[100px] h-[100px] rounded absolute'></motion.div> 
+        <motion.div 
+        animate={{rotate:360}}
+        transition={{repeat:Infinity,duration:1,type:'spring', ease:'easeInOut'}}
+        className='border border-white/30 w-[100px] h-[100px] rounded absolute'></motion.div> 
+        
+        </motion.div>    
+
+        <motion.div 
+        animate={{rotate:[360,45]}}
+        transition={{repeat:Infinity,duration:1,type:'spring', ease:'easeInOut'}}
+        className='border-5 border-white w-[200px] h-[200px] rounded-full absolute'>
+            <div className='border border-white/30 w-[50px] h-[50px] rounded-full'></div>
+            <div className='border border-white/30 w-[50px] h-[50px] rounded-full absolute bottom-0 right-0'></div>
+            <div className='border border-white/30 w-[50px] h-[50px] rounded-full absolute top-0 right-0'></div>
+            <div className='border border-white/30 w-[50px] h-[50px] rounded-full absolute left-0 bottom-0'></div>    
+        </motion.div>    
+
+        
+    </div>
+
+    </div>
+)
+}
+
+export default Loading
--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,53 @@
-import Link from "next/link";
+'use client'
 
-export default function Home() {
+import Link from "next/link";
+import {motion} from 'motion/react';
+import { useEffect, useState } from "react";
+import Loading from "./dashboard/Loading";
+
+
+export default function Home({children}) {
+
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(()=>{
+    const handleLoad = () =>{
+      setTimeout(() => {
+        setIsLoaded(true);
+      }, 3000);
+    };
+
+    if(document.readyState === 'complete'){
+      handleLoad();
+    }else{
+      window.addEventListener('load',handleLoad);
+    }
+    return() => window.removeEventListener('load',handleLoad);
+  },[]);
+
+
   return (
+    <>
+    {!isLoaded ?
+      <div>
+        <Loading/>
+      </div>
+    : 
     <main className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="md:text-4xl text-2xl font-bold ">Welcome to the Loader Demo</h1>
-      <p className="mb-6 text-neutral-400">You can try different types of loading animation. build using motion</p>
+      <p className="mb-7 px-10 text-neutral-400 w-full text-center">You can try different types of loading animation. build using motion</p>
       <Link href="/LoadersComponents" className="bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-600">
         Go to Loaders
       </Link>
 
-      <p className="mt-6 text-red-400">Roatate for better Expirence</p>
+      <motion.p 
+      animate={{opacity:[0,10,0]}}
+      transition={{repeat:Infinity,duration:2}}
+      className="mt-6 text-red-400">Roatate for better Expirence</motion.p>
     </main>
+     }
+
+    
+    </>
   );
 }
